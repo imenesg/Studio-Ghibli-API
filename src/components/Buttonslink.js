88065fc8d662ae3e4022fe8ec8e29397ea4d35a8
imenesg <imenesgabriel@gmail.com>
2022-05-24 12:22:08 -0300

    Add links

--- src/components/Buttonslink.js
+++ src/components/Buttonslink.js
@@ -4,10 +4,10 @@ function Buttonslink(){
     return(
         <div className="buttons">
             
-            <a target="_blank" rel="noopener noreferrer" href="https://www.netflix.com/browse/genre/81227213" >watch</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.netflix.com/browse/genre/81227213" >WATCH</a>
             
             
-                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/37i9dQZF1DX7GTqMQDhOum?si=398efb69b79042d8">listen</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://open.spotify.com/playlist/37i9dQZF1DX7GTqMQDhOum?si=398efb69b79042d8">LISTEN</a>
             
         </div>
     )
